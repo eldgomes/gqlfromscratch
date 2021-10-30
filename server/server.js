cfca57b2b15a397951ec0c0797c34bf39c6613b0
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-10-30 14:45:26 +0530

    Hidden root element -> schema

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+    schema { 
+        query: Query
+    }
+
     type Query {
         greeting: String
     }
@@ -12,12 +16,9 @@ const resolvers = {
     }
 }
 
-// resolvers need to mirror typedefinition precicely
-// Query(line 4) => Query(line 10)
-
 const server = new ApolloServer({typeDefs, resolvers});
 server.listen({port: 9000})
     .then((serverInfo) => console.log(`server running at ${serverInfo.url}`));
 
-
-// run by opening terminal, `node server.js`
+// playground `query` matches `Query` in schema
+// line(4) schema is the invisible entry point or the root element
