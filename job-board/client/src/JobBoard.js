3d4a432240eeba980cdc05926c969e6ab62d79d2
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-10-31 12:57:07 +0530

    Make graphql API call and update UI

--- job-board/client/src/JobBoard.js
+++ job-board/client/src/JobBoard.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { JobList } from './JobList';
-const { jobs } = require('./fake-data');
+import { loadJobs } from './requests';
 
 export class JobBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {jobs: []};
+  }
+
+  // call graphql API
+  // this function is called when component has mounted or basically when its displayed on the page
+  async componentDidMount() {
+    const jobs = await loadJobs();
+    this.setState({ jobs });
+  }
+
   render() {
+    const { jobs } = this.state;
     return (
       <div>
         <h1 className="title">Job Board</h1>
