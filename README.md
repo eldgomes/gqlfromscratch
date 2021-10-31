f16156a3d0c227f60533dbf982fedd401a5970d8
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-10-31 20:37:49 +0530

    Basic mutation

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-You can go any levels deeper with the nesting in queries
-Calls resolvers multiple times
-Can be bad on db
+Very basic mutation
--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -6,6 +6,12 @@ const Query = {
     jobs: () => db.jobs.list()
 };
 
+const Mutation = {
+    createJob: (root, {comapnyId, title, description}) => {
+        return db.jobs.create({comapnyId, title, description}); //returns an id
+    } 
+}
+
 // all resolver functions reciev parent argument (`job` in  this case)
 // the job object passed here is the result of invoking the parent resolver `jobs`
 const Job = {
@@ -17,4 +23,4 @@ const Company = {
         .filter(job => job.companyId === company.id)
 }
 
-module.exports = { Query, Job, Company };
+module.exports = { Query, Mutation, Job, Company };
