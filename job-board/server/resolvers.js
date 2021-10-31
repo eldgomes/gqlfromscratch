3220413cca1be90bdbbab82d55dff215b1829011
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-10-31 12:40:57 +0530

    Resolving nested queries

--- job-board/server/resolvers.js
+++ job-board/server/resolvers.js
@@ -4,4 +4,10 @@ const Query = {
     jobs: () => db.jobs.list()
 };
 
+// all resolver functions reciev parent argument (`job` in  this case)
+// the job object passed here is the result of invoking the parent resolver `jobs`
+const Job = {
+    company: job => db.companies.get(job.companyId)
+}
+
 module.exports = { Query };
