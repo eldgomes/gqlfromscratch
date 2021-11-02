cf8beffea7b8fb63ff80c066bcb53fa7cad6ab7b
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-11-02 10:01:49 +0530

    Graphql fragments

--- job-board/client/src/queries.js
+++ job-board/client/src/queries.js
@@ -1,5 +1,17 @@
 import gql from 'graphql-tag'; //tag fn are introduced in es6 along with template strings, strings processed by gql fn
 
+const JobDetailFragment = gql`
+    fragment JobDetail on Job {
+        id
+        title
+        company {
+            id
+            name
+        }
+        description
+    }
+`;
+
 export const loadJobsQuery = gql`query JobsQuery{
     jobs {
         id
@@ -13,14 +25,9 @@ export const loadJobsQuery = gql`query JobsQuery{
 
 export const loadJobQuery = gql`query JobQuery($id: ID!) {
     job(id: $id) {
-        id
-        title
-        company {
-            id
-            name
-        }
-        description
+        ...JobDetail
     }
+    ${JobDetailFragment}
 }`;
 
 export const loadCompanyQuery = gql`query CompanyQuery($id: ID!) {
@@ -38,11 +45,7 @@ export const loadCompanyQuery = gql`query CompanyQuery($id: ID!) {
 // job is an alias for the object name which otherwise would be createJob
 export const createJobMutation = gql`mutation CreateJob($input: CreateJobInput) {
     job: createJob(input: $input) {
-        id
-        title
-        company {
-            id
-            name
-        }
+        ...JobDetail
     }
+    ${JobDetailFragment}
 }`;
