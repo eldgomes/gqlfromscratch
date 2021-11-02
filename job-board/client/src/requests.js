36e772c89ad7efc1d350539310304a5420f0d83b
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-11-02 09:47:17 +0530

    Updating cache after mutation

--- job-board/client/src/requests.js
+++ job-board/client/src/requests.js
@@ -25,21 +25,33 @@ const client = new ApolloClient({
 });
 
 export async function loadJobs() {
-    const { data } = await client.query({loadJobsQuery, fetchPolicy: 'no-cache'}) //client object has query method used to send request
+    const { data } = await client.query({query: loadJobsQuery, fetchPolicy: 'no-cache'}) //client object has query method used to send request
     return data.jobs;
 }
 
 export async function loadJob(id) {
-    const { data } = await client.query({loadJobQuery, variables: { id }});
+    const { data } = await client.query({query: loadJobQuery, variables: { id }});
     return data.job;
 }
 
 export async function loadCompany(id) {
-    const { data } = await client.query({loadCompanyQuery, variables: { id }});
+    const { data } = await client.query({query: loadCompanyQuery, variables: { id }});
     return data.company;
 }
 
 export async function createJob(input) {
-    const { job } = await client.mutate({createJobMutation, variables: { input }});
+    const { job } = await client.mutate({
+        createJobMutation, 
+        variables: { input },
+        update: (cache, mutationResult) => { //called after the mutation has been executed , get resp and save in cache as if result of running the job query
+            cache.writeQuery({
+                query: loadJobQuery, 
+                variables: {id: mutationResult.data.job.id},
+                data: mutationResult.data
+            })  //writeQuery save results of a query
+        }
+        //cache~store or proxy
+        //mutationResult ->result from server
+    });
     return job;
 }
