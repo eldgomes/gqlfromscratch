6321709b5d01c9b1d809b9373cc1f4320a020db2
Eldridge Gomes <gomes.eldridge@gmail.com>
2021-11-03 17:05:21 +0530

    Apollo hooks: useSubscription

--- chat/client/src/Chat.js
+++ chat/client/src/Chat.js
@@ -1,19 +1,25 @@
-import { useMutation, useQuery } from '@apollo/client';
-import React from 'react';
+import { useMutation, useQuery, useSubscription } from '@apollo/client';
+import React, {useState} from 'react';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
-import { messagesQuery, addMessageMutation } from './graphql/queries';
+import { messagesQuery, addMessageMutation, messageAddedSubscription } from './graphql/queries';
 
 const Chat = (props) => {
   const {user} = props;
 
-  const result = useQuery(messagesQuery); //rendered twice, first time data is null, second time it loads it
-  // also pass veriables, fetchpoolicy in useQuery, 
-  // get loading out out of it as well to show loader, get error as well
+  const [messages, setMessages] = useState([]);
+
+  useQuery(messagesQuery, {
+    onCompleted: data => setMessages(data.messages)
+  }); 
+
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: (result) => {
+      setMessages(message.concat(result.subscriptionData.data.messageAdded));
+    }
+  }); // handles closing subscriptions
 
   const [addMessage, result]= useMutation(addMessageMutation); //return a fn and a result obj (has loading, error, data, called props)
-
-  const messages = result.data ? result.data.messages : [];
 
   const handleSend = async (text) => {
     /*const {data} =*/ await addMessage({variables: {input: {text}}}); //dont need to do anything with data
